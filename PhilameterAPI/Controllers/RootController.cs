930a9ee60a288272e5417d9644db368f11d62d53
bowersad <bowersad@gmail.com>
2017-08-20 11:17:25 -0400

    Initial Commit

---
+++ PhilameterAPI/Controllers/RootController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhilameterAPI.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var result = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                value1 = "value1"
+            };
+
+            return Ok(result);
+            
+        }
+    }
+}
